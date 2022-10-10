7560d05fbd3343f3e53130e8504d3d87a247757f
Benjamin Bojko <benjaminbojko@users.noreply.github.com>
2022-10-10 17:31:01 -0400

    Added example for monitoring a .bat file

--- packages/monitor/test/config.js
+++ packages/monitor/test/config.js
@@ -1,42 +1,60 @@
 const config = {
-	"apps": [
-		{
-			"pm2": {
-				"name": "ping bluecadet.com",
-				"script": "cmd.exe",
-				"args": "/c ping -n -1 bluecadet.com",
-			},
-			"windows": {
-				"minimize": false,
-				"foreground": true,
-				"hide": false,
-			},
-			"logging": {
-				"logToLaunchpadDir": true,
-				"showStdout": true,
-				"showStderr": true,
-				"mode": "file"
-			}
-		},
-		{
-			"pm2": {
-				"name": "ping google.com",
-				"script": "cmd.exe",
-				"args": "/c ping -n -1 google.com",
-			},
-			"windows": {
-				"minimize": true,
-				"foreground": false,
-				"hide": false,
-			},
-			"logging": {
-				"logToLaunchpadDir": true,
-				"showStdout": true,
-				"showStderr": true,
-				"mode": "bus"
-			}
-		},
-	]
+  "apps": [
+    {
+      "pm2": {
+        "name": "ping bluecadet.com",
+        "script": "cmd.exe",
+        "args": "/c ping -n -1 bluecadet.com",
+      },
+      "windows": {
+        "minimize": false,
+        "foreground": true,
+        "hide": false,
+      },
+      "logging": {
+        "logToLaunchpadDir": true,
+        "showStdout": true,
+        "showStderr": true,
+        "mode": "file"
+      }
+    },
+    {
+      "pm2": {
+        "name": "ping google.com",
+        "script": "cmd.exe",
+        "args": "/c ping -n -1 google.com",
+      },
+      "windows": {
+        "minimize": true,
+        "foreground": false,
+        "hide": false,
+      },
+      "logging": {
+        "logToLaunchpadDir": true,
+        "showStdout": true,
+        "showStderr": true,
+        "mode": "bus"
+      }
+    },
+    {
+      "pm2": {
+        "name": "ping bluecadet.com via bat",
+        "script": "cmd.exe",
+        "args": "/c test\\ping.bat",
+      },
+      "windows": {
+        "minimize": false,
+        "foreground": true,
+        "hide": false,
+      },
+      "logging": {
+        "logToLaunchpadDir": true,
+        "showStdout": true,
+        "showStderr": true,
+        "mode": "file"
+      }
+    },
+  ]
 };
 
 export default config;

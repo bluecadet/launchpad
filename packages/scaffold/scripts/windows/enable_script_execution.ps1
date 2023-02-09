cb7ae938d11f0d9f6f0e4b9403ea5a29a90cfaed
Benjamin Bojko <benjaminbojko@users.noreply.github.com>
2023-02-09 11:48:54 -0500

    Fixed script exec script error

--- packages/scaffold/scripts/windows/enable_script_execution.ps1
+++ packages/scaffold/scripts/windows/enable_script_execution.ps1
@@ -6,5 +6,3 @@ force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows"
 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "EnableScripts" -Type DWord -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" -Name "ExecutionPolicy" -Type String -Value "RemoteSigned"
-
-HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows

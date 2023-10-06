bfb867645d0ada6e01840f70874dd771feecfd21
Clay Tercek <clay.tercek@gmail.com>
2023-10-06 12:57:30 -0400

    allow different context for different hooks

--- packages/content/lib/content-plugin.js
+++ packages/content/lib/content-plugin.js
@@ -1,33 +1,71 @@
+import { HookContextProvider } from '@bluecadet/launchpad-utils/lib/plugin-driver.js';
 import { DataFile } from './content-sources/content-result.js';
 import mdToHtml from './plugins/md-to-html.js';
 import sanityToHtml from './plugins/sanity-to-html.js';
 import sanityToMd from './plugins/sanity-to-markdown.js';
 import sanityToPlain from './plugins/sanity-to-plain.js';
+import MediaDownloader from './utils/media-downloader.js';
+
+/**
+ * @typedef ContentHookContext
+ * @prop {MediaDownloader} mediaDownloader
+ * // TBD 
+ */
+
+/**
+ * @typedef {import('@bluecadet/launchpad-utils/lib/plugin-driver.js').BaseHookContext & ContentHookContext} CombinedContentHookContext
+ */
 
 /**
  * @typedef ContentHooks
- * @prop {() => void} onContentFetchSetup Called before a content source is fetched
- * @prop {(param: {dataFiles: DataFile[]}) => void} onContentFetchData Called when all content for a given source has been fetched
- * @prop {() => void} onContentFetchError Called when a content source fails to fetch
+ * @prop {(ctx: CombinedContentHookContext, ) => void} onContentFetchSetup Called before a content source is fetched
+ * @prop {(ctx: CombinedContentHookContext, param: {dataFiles: DataFile[]}) => void} onContentFetchData Called when all content for a given source has been fetched
+ * @prop {(ctx: CombinedContentHookContext, ) => void} onContentFetchError Called when a content source fails to fetch
  */
 
 /**
- * @typedef {import("@bluecadet/launchpad-utils/lib/plugin-driver.js").Plugin<ContentHooks>} ContentPlugin
+ * @typedef {import('@bluecadet/launchpad-utils/lib/plugin-driver.js').Plugin<ContentHooks>} ContentPlugin
  */
 
-// hack to make sure typescript sees this file
-export {};
+/**
+ * @extends {HookContextProvider<ContentHooks, ContentHookContext>}
+ */
+export class ContentPluginDriver extends HookContextProvider {
+	/**
+	 * @type {MediaDownloader}
+	 */
+	#mediaDownloader;
+
+	/**
+	 * @param {import('@bluecadet/launchpad-utils/lib/plugin-driver.js').default<ContentHooks>} wrappee
+	 * @param {object} options
+	 * @param {MediaDownloader} options.mediaDownloader
+	 */
+	constructor(wrappee, { mediaDownloader }) {
+		super(wrappee);
+		this.#mediaDownloader = mediaDownloader;
+	}
+
+	/**
+	 * @override
+	 */
+	_getPluginContext() {
+		return {
+			mediaDownloader: this.#mediaDownloader
+		};
+	}
+}
 
 /**
  * @param {import('./content-options.js').ResolvedContentOptions} options
- * @returns {ContentPlugin[]}
+ * @returns {import('@bluecadet/launchpad-utils/lib/plugin-driver.js').Plugin<ContentHooks>[]}
  * @deprecated this is a temporary solution to add compatibility with the old config style. Once JS configs are supported, this will be removed.
  */
 export function createPluginsFromConfig(options) {
 	const transforms = options.contentTransforms;
 
 	/**
-   * @type {ContentPlugin[]}
+   * @type {import('@bluecadet/launchpad-utils/lib/plugin-driver.js').Plugin<ContentHooks>[]}
    */
 	const plugins = [];
 
--- packages/utils/lib/plugin-driver.js
+++ packages/utils/lib/plugin-driver.js
@@ -6,25 +6,35 @@ import LogManager from './log-manager.js';
  */
 
 /**
- * @typedef PluginContext
+ * @typedef BaseHookContext
  * @property {import('./log-manager.js').Logger} logger
  */
 
 /**
- * @template {unknown[]} [T=never[]]
- * @typedef {(...args: T) => void | Promise<void>} Hook
+ * @typedef {Record<string, (ctx: any, ...args: never[]) => void | Promise<void>>} HookSet
  */
 
 /**
- * @template {Record<string, Hook>} T
+ * @template {HookSet} T
  * @typedef Plugin
  * @property {string} name
- * @property {{[K in keyof T]?: (ctx: PluginContext, ...args: Parameters<T[K]>) => void | Promise<void>}} hooks
+ * @property {Partial<T>} hooks
+ */
+
+/**
+ * @template {unknown[]} T
+ * @typedef {T extends [unknown, ...infer R] ? R : []} Tail Omit the first element of a tuple
+ */
+
+/**
+ * @template {HookSet} T
+ * @template C
+ * @typedef {{ [K in keyof T]: C extends Parameters<T[K]>[0] ? K : never }[keyof T]} KeysWithFullContext
  */
 
 /**
  * Manage plugins and run hooks.
- * @template {Record<string, Hook>} T
+ * @template {HookSet} T
  */
 export default class PluginDriver {
 	/**
@@ -33,10 +43,17 @@ export default class PluginDriver {
 	#plugins = [];
 
 	/**
-	 * @readonly
-	 * @type {Map<Plugin<T>, PluginContext>}
+	 * @returns {ReadonlyArray<Plugin<T>>}
 	 */
-	#pluginContexts = new Map();
+	get plugins() {
+		return this.#plugins;
+	}
+
+	/**
+	 * @readonly
+	 * @type {Map<Plugin<T>, BaseHookContext>}
+	 */
+	#baseHookContexts = new Map();
 
 	/**
 	 * @param {Plugin<T>[]} plugins
@@ -54,30 +71,121 @@ export default class PluginDriver {
 
 		for (const plugin of pluginArray) {
 			this.#plugins.push(plugin);
-			this.#pluginContexts.set(plugin, {
+			this.#baseHookContexts.set(plugin, {
 				logger: LogManager.getInstance().getLogger(`plugin:${plugin.name}`)
 			});
 		}
 	}
 
 	/**
-	 * Run a hook in all plugins sequentially, passing the same argument to each.
+	 * @param {Plugin<T>} plugin
+	 */
+	#getBaseContext(plugin) {
+		const ctx = this.#baseHookContexts.get(plugin);
+
+		if (!ctx) {
+			throw new Error(`Plugin not found: ${plugin.name}`);
+		}
+
+		return ctx;
+	}
+
+	/**
 	 * @template {keyof T} K
 	 * @param {K} hookName
-	 * @param {Parameters<T[K]>} args
+	 * @param {(plugin: Plugin<T>) => Omit<Parameters<T[K]>[0], keyof BaseHookContext>} contextGetter
+	 * @param {Tail<Parameters<T[K]>>} additionalArgs
+	 * @returns {Promise<void>}
 	 */
-	async runHookSequential(hookName, ...args) {
+	async _runHookSequentialWithCtx(hookName, contextGetter, additionalArgs) {
 		for (const plugin of this.#plugins) {
 			const hook = plugin.hooks[hookName];
 			if (hook) {
-				const ctx = this.#pluginContexts.get(plugin);
-
-				if (!ctx) {
-					throw new Error(`Plugin context not found for plugin ${plugin.name}`);
-				}
-				
-				await hook(ctx, ...args);
+				const context = {
+					...this.#getBaseContext(plugin),
+					...contextGetter(plugin)
+				};
+				await hook(context, ...additionalArgs);
 			}
 		}
+	}
+
+	/**
+	 * @template {KeysWithFullContext<T, BaseHookContext>} K
+	 * @param {K} hookName
+	 * @param {Tail<Parameters<T[K]>>} additionalArgs
+	 */
+	async runHookSequential(hookName, ...additionalArgs) {
+		for (const plugin of this.#plugins) {
+			const hook = plugin.hooks[hookName];
+			if (hook) {
+				await hook(this.#getBaseContext(plugin), ...additionalArgs);
+			}
+		}
+	}
+}
+
+/**
+ * Base class for injecting additional context when running hooks.
+ * @template {HookSet} T Hooks
+ * @template C Context
+ */
+export class HookContextProvider {
+	/**
+	 * @type {PluginDriver<T>}
+	 */
+	#innerDriver;
+
+	/**
+	 * @param {PluginDriver<T>} innerDriver
+	 */
+	constructor(innerDriver) {
+		this._initialize(innerDriver.plugins);
+		this.#innerDriver = innerDriver;
+	}
+
+	get plugins() {
+		return this.#innerDriver.plugins;
+	}
+
+	/**
+	 * @param {ReadonlyArray<Plugin<T>>} plugins
+	 * @protected
+	 */
+	_initialize(plugins) {
+		// implement in subclass
+	}
+
+	/**
+	 * @param {Plugin<T>} plugin
+	 * @returns {C}
+	 * @protected
+	 */
+	_getPluginContext(plugin) {
+		throw new Error('_getPluginContext Not implemented');
+	}
+
+	/**
+	 * add a plugin or array of plugins to the driver
+	 * @param {Plugin<T> | Plugin<T>[]} plugins 
+	 */
+	add(plugins) {
+		const pluginArray = Array.isArray(plugins) ? plugins : [plugins];
+		this._initialize(pluginArray);
+		this.#innerDriver.add(plugins);
+	}
+
+	/**
+	 * @template {KeysWithFullContext<T, C & BaseHookContext>} K
+	 * @param {K} hookName 
+	 * @param  {Tail<Parameters<T[K]>>} additionalArgs 
+	 * @returns 
+	 */
+	async runHookSequential(hookName, ...additionalArgs) {
+		return this.#innerDriver._runHookSequentialWithCtx(
+			hookName,
+			this._getPluginContext,
+			additionalArgs
+		);
 	}
 }

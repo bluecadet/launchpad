3137a48c6fb8b3bed124f292009ca3bdddabc36d
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2024-09-12 12:48:32 -0400

    fix jsdoc type imports

--- packages/launchpad/lib/launchpad-options.js
+++ packages/launchpad/lib/launchpad-options.js
@@ -3,17 +3,17 @@
  */
 
 /**
- * @typedef {import("@bluecadet/launchpad-content/lib/content-plugin-driver").ContentHooks & import("@bluecadet/launchpad-monitor/lib/monitor-plugin-driver").MonitorHooks} AllHooks
+ * @typedef {import("@bluecadet/launchpad-content/lib/content-plugin-driver.js").ContentHooks & import("@bluecadet/launchpad-monitor/lib/monitor-plugin-driver.js").MonitorHooks} AllHooks
  */
 
 /**
  * @typedef LaunchpadOptions Combined options to initialize Launchpad.
  * @property {import("@bluecadet/launchpad-content").ContentOptions} [content]
  * @property {import("@bluecadet/launchpad-monitor").MonitorOptions} [monitor]
- * @property {import("./command-center").CommandOptions} [commands]
- * @property {import("./command-hooks").HookMapping} [hooks]
- * @property {import("@bluecadet/launchpad-utils/lib/plugin-driver").Plugin<AllHooks>[]} [plugins]
- * @property {import("@bluecadet/launchpad-utils/lib/log-manager").LogOptions} [logging]
+ * @property {import("./command-center.js").CommandOptions} [commands]
+ * @property {import("./command-hooks.js").HookMapping} [hooks]
+ * @property {import("@bluecadet/launchpad-utils/lib/plugin-driver.js").Plugin<AllHooks>[]} [plugins]
+ * @property {import("@bluecadet/launchpad-utils/lib/log-manager.js").LogOptions} [logging]
  * @property {boolean} [shutdownOnExit] Will listen for exit events. Defaults to 'true'
  */
 

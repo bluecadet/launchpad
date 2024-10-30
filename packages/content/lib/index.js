a37a24093cedce25982a1245a78144b66c83e98a
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2024-10-30 18:31:49 -0400

    fix content exports

--- packages/content/lib/index.js
+++ packages/content/lib/index.js
@@ -5,4 +5,7 @@ import { LaunchpadContent } from './launchpad-content.js';
 export * from './content-options.js';
 export * from './launchpad-content.js';
 export * from './utils/file-utils.js';
+export * from './sources/index.js';
+export * from './plugins/index.js';
+
 export default LaunchpadContent;

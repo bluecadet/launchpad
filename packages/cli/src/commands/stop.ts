03b7d6a14d9d5cb1601dcc79ac890d056a73e712
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2024-11-05 20:43:23 -0500

    migrate cli to ts

---
+++ packages/cli/src/commands/stop.ts
@@ -0,0 +1,8 @@
+import LaunchpadMonitor from "@bluecadet/launchpad-monitor";
+import { LogManager } from "@bluecadet/launchpad-utils";
+import type { LaunchpadArgv } from "../cli.js";
+
+export async function stop(argv: LaunchpadArgv) {
+	const logger = LogManager.configureRootLogger();
+	await LaunchpadMonitor.kill(logger);
+}
---
+++ packages/cli/src/errors.ts
@@ -0,0 +1,27 @@
+export class LaunchpadCLIError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = "LaunchpadCLIError";
+	}
+}
+
+export class ImportError extends LaunchpadCLIError {
+	constructor(message: string) {
+		super(message);
+		this.name = "ImportError";
+	}
+}
+
+export class ConfigError extends LaunchpadCLIError {
+	constructor(message: string) {
+		super(message);
+		this.name = "ConfigError";
+	}
+}
+
+export class MonitorError extends LaunchpadCLIError {
+	constructor(message: string) {
+		super(message);
+		this.name = "MonitorError";
+	}
+}

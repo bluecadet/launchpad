5f53829e1bb27add9d57f0456e9b2b9e23f91db0
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2024-11-23 12:57:29 -0500

    add fetch logger

--- packages/content/src/utils/fetch-logger.ts
+++ packages/content/src/utils/fetch-logger.ts
@@ -1,9 +1,92 @@
-import { FixedLogger } from "@bluecadet/launchpad-utils";
+import { FixedConsoleLogger, NO_TTY } from "@bluecadet/launchpad-utils";
+import chalk from "chalk";
 
-export class FetchLogger extends FixedLogger {
-  constructor(sources) {
-    super();
-  }
+type FetchData = Promise<unknown> | AsyncIterable<unknown>;
 
+const spinnerFrames = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
+type FetchState =
+	| {
+			state: "pending";
+	  }
+	| {
+			state: "resolved";
+			duration: number;
+	  }
+	| {
+			state: "rejected";
+			duration: number;
+	  };
+
+export class FetchLogger extends FixedConsoleLogger {
+	#fetches: Map<string, Map<string, FetchState>> = new Map();
+	#spinnerFrameIndex = 0;
+
+	addFetch(sourceId: string, documentId: string, fetchPromise: FetchData): void {
+		if (!this.#fetches.has(sourceId)) {
+			this.#fetches.set(sourceId, new Map());
+		}
+
+		this.#fetches.get(sourceId)?.set(documentId, {
+			state: "pending",
+		});
+
+		const startTime = Date.now();
+
+		if (fetchPromise instanceof Promise) {
+			fetchPromise
+				.then(() => {
+					const endTime = Date.now();
+					const duration = endTime - startTime;
+					this.#fetches.get(sourceId)?.set(documentId, { state: "resolved", duration });
+					this.logger.info(
+						`${this.getIcon("resolved")} Fetched ${documentId} from ${sourceId} in ${duration}ms`,
+						{
+							[NO_TTY]: true,
+						},
+					);
+				})
+				.catch(() => {
+					const endTime = Date.now();
+					const duration = endTime - startTime;
+					this.#fetches.get(sourceId)?.set(documentId, { state: "rejected", duration });
+					this.logger.error(
+						`${this.getIcon("rejected")} Fetched ${documentId} from ${sourceId} in ${duration}ms`,
+						{
+							[NO_TTY]: true,
+						},
+					);
+				});
+		}
+	}
+
+	getIcon(state: FetchState["state"]) {
+		switch (state) {
+			case "pending":
+				return chalk.cyan(spinnerFrames[this.#spinnerFrameIndex]);
+			case "resolved":
+				return chalk.green`✓`;
+			case "rejected":
+				return chalk.red`✗`;
+		}
+	}
+
+	override getFixedConsoleMessage(): string {
+		this.#spinnerFrameIndex = (this.#spinnerFrameIndex + 1) % spinnerFrames.length;
+
+		let message = "";
+		for (const [sourceId, documentFetches] of this.#fetches) {
+			for (const [documentId, fetchState] of documentFetches) {
+				message += ` ${this.getIcon(fetchState.state)} ${chalk.gray(`${sourceId}/`)}${documentId}`;
+
+				if (fetchState.state !== "pending") {
+					message += chalk.gray(` (${fetchState.duration}ms)`);
+				}
+
+				message += "\n";
+			}
+		}
+
+		return message;
+	}
 }
--- packages/utils/src/console-transport.ts
+++ packages/utils/src/console-transport.ts
@@ -5,10 +5,10 @@ import { onExit } from "./on-exit.js";
 import type { TransformableInfo, Format } from "logform";
 import type { Logger } from "./log-manager.js";
 
-export const TTY_ONLY: unique symbol = Symbol('ttyOnly');
-export const NO_TTY: unique symbol = Symbol('noTTY');
-export const TTY_FIXED: unique symbol = Symbol('ttyFixed');
-export const TTY_FIXED_END: unique symbol = Symbol('ttyFixedEnd');
+export const TTY_ONLY = Symbol("ttyOnly");
+export const NO_TTY = Symbol("noTTY");
+export const TTY_FIXED = Symbol("ttyFixed");
+export const TTY_FIXED_END = Symbol("ttyFixedEnd");
 
 /**
  * Custom winston transport that allows for fixed messages to be displayed on the console.
@@ -24,104 +24,109 @@ export class CustomConsoleTransport extends winston.transports.Console {
 
 	#fixedMessage: string | null = null;
 
-	override log(info: {message: string} & Record<string | symbol, unknown>, callback: () => void) {
-			if (info[TTY_FIXED] === true) {
-				this.#updateFixedMessage(info.message);
+	override log(info: { message: string } & Record<string | symbol, unknown>, callback: () => void) {
+		if (info[TTY_FIXED] === true) {
+			this.#updateFixedMessage(info.message);
+			callback();
+			return;
+		}
+
+		if (info[TTY_FIXED_END] === true) {
+			this.#updateFixedMessage(null);
+			callback();
+			return;
+		}
+
+		if (this.#fixedMessage !== null) {
+			const lines = this.#fixedMessage.split("\n").length;
+			process.stdout.write(ansiEscapes.eraseLines(lines));
+		}
+		if (super.log) {
+			super.log(info, () => {
+				// after normal log, log the fixed message again
+				if (this.#fixedMessage !== null) {
+					process.stdout.write(ansiEscapes.eraseDown);
+					process.stdout.write(this.#fixedMessage);
+				}
+
 				callback();
-				return;
-			}
-
-			if (info[TTY_FIXED_END] === true) {
-				this.#updateFixedMessage(null);
-				callback();
-				return;
-			}
-
-			if (this.#fixedMessage !== null) {
-					const lines = this.#fixedMessage.split('\n').length;
-					process.stdout.write(ansiEscapes.eraseLines(lines));
-			}
-			if (super.log) {
-					super.log(info, () => {
-            // after normal log, log the fixed message again
-            if (this.#fixedMessage !== null) {
-              process.stdout.write(ansiEscapes.eraseDown);
-              process.stdout.write(this.#fixedMessage);
-            }
-
-            callback();
-          });
-			}
+			});
+		}
 	}
 	#updateFixedMessage(message: string | null) {
 		const lastMessage = this.#fixedMessage;
-			if (lastMessage === null) {
-				// hide cursor when displaying fixed message
-				process.stdout.write(ansiEscapes.cursorHide);
-			}
+		if (lastMessage === null) {
+			// hide cursor when displaying fixed message
+			process.stdout.write(ansiEscapes.cursorHide);
+		}
 
-			if (message === null) {
-				process.stdout.write(ansiEscapes.cursorShow);
-			} else {
-				const lastLineCount = lastMessage?.split('\n').length || 0;
-				process.stdout.write(ansiEscapes.eraseLines(lastLineCount)); // erase last message
-				process.stdout.write(message);
-			}
+		if (message === null) {
+			process.stdout.write(ansiEscapes.cursorShow);
+		} else {
+			const lastLineCount = lastMessage?.split("\n").length || 0;
+			process.stdout.write(ansiEscapes.eraseLines(lastLineCount)); // erase last message
+			process.stdout.write(message);
+		}
 
-			this.#fixedMessage = message;
+		this.#fixedMessage = message;
 	}
 
-	#handleQuit ()  {
+	#handleQuit() {
 		// make sure to show cursor before exiting
 		process.stdout.write(ansiEscapes.cursorShow);
-	};
+	}
 }
 
-
 export class FilterLogType implements Format {
-	constructor(private readonly type: 'file' | 'tty') {}
+	constructor(private readonly type: "file" | "tty") {}
 
 	transform(info: TransformableInfo) {
 		const isTTyLog = !!info[TTY_ONLY] || !!info[TTY_FIXED] || !!info[TTY_FIXED_END];
 		const isNoTTYLog = !!info[NO_TTY];
 
 		// remove ttyFixed and ttyFixedEnd from file logs
-		if (this.type === 'file' && isTTyLog) {
+		if (this.type === "file" && isTTyLog) {
 			return false;
 		}
 
-		if (this.type === 'tty' && isNoTTYLog) {
+		if (this.type === "tty" && isNoTTYLog) {
 			return false;
 		}
 
 		// remove tty props, as these don't need to be logged in files or tty
 		info[TTY_ONLY] = undefined;
 		info[NO_TTY] = undefined;
-		info[TTY_FIXED] = undefined;
-		info[TTY_FIXED_END] = undefined;
 
 		return info;
 	}
 }
 
 export abstract class FixedConsoleLogger {
-  #interval: NodeJS.Timeout;
+	#interval: NodeJS.Timeout;
 
-  constructor(protected logger: Logger, level = 'info', ttyUpdateInterval = 100) {
-    this.#interval = setInterval(() => {
-      const consoleMessage = this.getFixedConsoleMessage();
-			logger.log({
-				level,
-				message: consoleMessage,
-				[TTY_FIXED]: true
-			});
-    }, ttyUpdateInterval);
-  }
+	constructor(
+		protected logger: Logger,
+		level = "info",
+		ttyUpdateInterval = 50,
+	) {
+		this.#interval = setInterval(() => {
+			this.update();
+		}, ttyUpdateInterval);
+	}
 
-  abstract getFixedConsoleMessage(): string;
+	abstract getFixedConsoleMessage(): string;
 
-  close() {
-    clearInterval(this.#interval);
-		this.logger.info('', { [TTY_FIXED_END]: true });
-  };
+	update() {
+		this.logger.log({
+			level: "info",
+			message: this.getFixedConsoleMessage(),
+			[TTY_FIXED]: true,
+		});
+	}
+
+	close() {
+		clearInterval(this.#interval);
+		this.update();
+		this.logger.info("", { [TTY_FIXED_END]: true });
+	}
 }

ea0e1ac900f7674ea5d2e49b83d12b9c9114ccf5
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2024-12-02 11:11:15 -0500

    remove old scaffold scripts

--- packages/scaffold/scripts/windows/disable_new_network_window.ps1
+++
@@ -1,4 +0,0 @@
-Write-Output "Disabling new network window"
-
-Import-Module -DisableNameChecking $PSScriptRoot/../vendor/force-mkdir.psm1
-force-mkdir "HKLM:\SYSTEM\currentControlSet\Control\Network\NewNetworkWindowOff"

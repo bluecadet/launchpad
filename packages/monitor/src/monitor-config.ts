1fd2721b1e1ff003c70bcd36351cbba3534384d7
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-04-03 16:08:44 -0400

    fix lint err

--- packages/monitor/src/monitor-config.ts
+++ packages/monitor/src/monitor-config.ts
@@ -1,6 +1,6 @@
+import type { StartOptions } from "pm2";
 import { z } from "zod";
 import { monitorPluginSchema } from "./core/monitor-plugin-driver.js";
-import type { StartOptions } from "pm2";
 
 const windowsApiConfigSchema = z.object({
 	/**

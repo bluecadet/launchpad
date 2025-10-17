e705057cb5f066aad1f550d625be3f82635d4644
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-10-17 10:30:59 -0400

    write tests for controller, add controller to launchpad main pkg

--- packages/launchpad/src/index.ts
+++ packages/launchpad/src/index.ts
@@ -1,3 +1,4 @@
 export * from "@bluecadet/launchpad-cli";
 export * from "@bluecadet/launchpad-content";
+export * from "@bluecadet/launchpad-controller";
 export * from "@bluecadet/launchpad-monitor";

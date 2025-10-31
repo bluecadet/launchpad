dc131930be052e5f72cf5b88d181373ef3b82ec1
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-10-31 09:03:37 -0700

    chore(controller): fix lint errors

--- packages/controller/src/ipc/ipc-utils.ts
+++ packages/controller/src/ipc/ipc-utils.ts
@@ -5,17 +5,14 @@ import path from "node:path";
  * https://nodejs.org/api/net.html#identifying-paths-for-ipc-connections
  */
 export function getOSSocketPath(socketPath: string) {
-  if (process.platform === "win32") {
-    // On Windows, we need to use a named pipe
+	if (process.platform === "win32") {
+		// On Windows, we need to use a named pipe
 
-    if (socketPath.includes("\\\\?\\pipe") || socketPath.includes("\\\\.\\pipe")) {
-      // Already in correct format
-      return socketPath;
-    }
-    return path.join(
-      '\\\\?\\pipe',
-      socketPath
-    )
-  }
-  return socketPath;
+		if (socketPath.includes("\\\\?\\pipe") || socketPath.includes("\\\\.\\pipe")) {
+			// Already in correct format
+			return socketPath;
+		}
+		return path.join("\\\\?\\pipe", socketPath);
+	}
+	return socketPath;
 }

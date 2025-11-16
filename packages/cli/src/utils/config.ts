e090eb19932320f13f7385892d6c73a1c31c4318
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-11-16 10:15:54 -0500

    fix(cli): simplify config file search
    we were previously searching for all valid config files... this was unecessary, as we were just using the first from the list. Now we just return the first found.

--- packages/cli/src/utils/config.ts
+++ packages/cli/src/utils/config.ts
@@ -5,7 +5,7 @@ import chalk from "chalk";
 import { createJiti } from "jiti";
 import { cliLogger } from "./cli-logger.js";
 
-const DEFAULT_CONFIG_PATHS = [
+const CONFIG_FILE_NAMES = [
 	"launchpad.config.js",
 	"launchpad.config.mjs",
 	"launchpad.config.ts",
@@ -15,31 +15,19 @@ const DEFAULT_CONFIG_PATHS = [
 ];
 
 /**
- * Searches for all config files in the current and parent directories, up to a max depth of 64.
- * @returns {string[]} Array of absolute paths to the config files found.
+ * Searches for config file in the current and parent directories, up to a max depth of 64.
+ * @returns {string | null} Absolute path to the first config file found, or null if none are found.
  */
-export function findConfig() {
-	const configs = findAllConfigsRecursive();
-	if (configs.length > 0) {
-		cliLogger.info(`Found configs: ${configs.map((c) => chalk.grey(c)).join(", ")}`);
-	}
-	return configs.length > 0 ? configs[0] : null;
-}
-
-/**
- * Searches for all config files in the current and parent directories, up to a max depth of 64.
- * @returns {string[]} Array of absolute paths to the config files found.
- */
-function findAllConfigsRecursive() {
+export function findFirstConfigRecursive(): string | null {
 	const maxDepth = 64;
-	const foundConfigs = [];
 	let currentDir = process.env.INIT_CWD ? path.resolve(process.env.INIT_CWD) : process.cwd();
 
 	for (let i = 0; i < maxDepth; i++) {
-		for (const defaultPath of DEFAULT_CONFIG_PATHS) {
-			const candidatePath = path.resolve(currentDir, defaultPath);
+		for (const filename of CONFIG_FILE_NAMES) {
+			const candidatePath = path.resolve(currentDir, filename);
 			if (fs.existsSync(candidatePath)) {
-				foundConfigs.push(candidatePath);
+				cliLogger.info(`Found config: ${chalk.gray(candidatePath)}`);
+				return candidatePath;
 			}
 		}
 
@@ -52,7 +40,7 @@ function findAllConfigsRecursive() {
 		currentDir = parentDir;
 	}
 
-	return foundConfigs;
+	return null;
 }
 
 export async function loadConfigFromFile<T>(configPath: string): Promise<Partial<T>> {

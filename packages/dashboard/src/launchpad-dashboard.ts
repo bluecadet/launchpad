38ccb0928b0a218cee61994fe6b15c537d5210b1
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-12-10 11:36:24 -0500

    fixup! fix(dashboard): load templates during dashboard registration instead of top level async fns

--- packages/dashboard/src/launchpad-dashboard.ts
+++ packages/dashboard/src/launchpad-dashboard.ts
@@ -25,58 +25,58 @@ export function createLaunchpadDashboard(
 
 			const _registry = new DashboardRegistryImpl(app);
 
-			return registerCallback(_registry).andThen(() => {
-				registerLogPanelFeatures(_registry, _ctx.eventBus);
+			return registerCallback(_registry)
+				.andThen(() => registerLogPanelFeatures(_registry, _ctx.eventBus))
+				.andThen(() => {
+					app.use((event) => {
+						// log requests if enabled
+						const { method, url } = event.req;
+						_ctx.logger.debug(`${method} ${url}`);
+					});
 
-				app.use((event) => {
-					// log requests if enabled
-					const { method, url } = event.req;
-					_ctx.logger.debug(`${method} ${url}`);
-				});
+					app.use(
+						onError((event, error) => {
+							const level = event.status && event.status >= 500 ? "error" : "warn";
 
-				app.use(
-					onError((event, error) => {
-						const level = event.status && event.status >= 500 ? "error" : "warn";
+							const msg = `[${chalk[level === "error" ? "red" : "yellow"](event.status)}] Error processing request: ${error.req.method} ${error.req.url}`;
+							if (level === "error") {
+								_ctx.logger.error({
+									message: new Error(msg, { cause: event }),
+								});
+							} else {
+								_ctx.logger.warn(msg);
+							}
+						}),
+					);
 
-						const msg = `[${chalk[level === "error" ? "red" : "yellow"](event.status)}] Error processing request: ${error.req.method} ${error.req.url}`;
-						if (level === "error") {
-							_ctx.logger.error({
-								message: new Error(msg, { cause: event }),
-							});
-						} else {
-							_ctx.logger.warn(msg);
+					// disable caching by default, except for static files served from the dashboard
+					app.use((event) => {
+						if (event.req.method === "GET" && !event.req.url?.startsWith("/static/")) {
+							event.res.headers.set(
+								"Cache-Control",
+								"no-store, no-cache, must-revalidate, proxy-revalidate",
+							);
 						}
-					}),
-				);
+					});
 
-				// disable caching by default, except for static files served from the dashboard
-				app.use((event) => {
-					if (event.req.method === "GET" && !event.req.url?.startsWith("/static/")) {
-						event.res.headers.set(
-							"Cache-Control",
-							"no-store, no-cache, must-revalidate, proxy-revalidate",
-						);
-					}
-				});
+					const server = serve(app, {
+						port: resolvedConfig.port,
+						hostname: resolvedConfig.host,
+						silent: true,
+						gracefulShutdown: false,
+						manual: false,
+					});
 
-				const server = serve(app, {
-					port: resolvedConfig.port,
-					hostname: resolvedConfig.host,
-					silent: true,
-					gracefulShutdown: false,
-					manual: false,
+					return okAsync({
+						// Expose the registry so the controller can pass it to other subsystems
+						getRegistry() {
+							return _registry;
+						},
+						disconnect() {
+							return ResultAsync.fromPromise(server.close(), (e) => e as Error);
+						},
+					});
 				});
-
-				return okAsync({
-					// Expose the registry so the controller can pass it to other subsystems
-					getRegistry() {
-						return _registry;
-					},
-					disconnect() {
-						return ResultAsync.fromPromise(server.close(), (e) => e as Error);
-					},
-				});
-			});
 		},
 	});
 }
